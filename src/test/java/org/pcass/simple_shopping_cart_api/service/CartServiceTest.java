b8d046a566f4955aee5f774660d337ba3d980fcd
Peter Cassidy <petercassidy85@gmail.com>
2026-03-18 01:41:19 +0100

    fix incorrect test file naming

--- src/test/java/org/pcass/simple_shopping_cart_api/service/CartServiceTest.java
+++ src/test/java/org/pcass/simple_shopping_cart_api/service/CartServiceTest.java
@@ -17,7 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class CartControllerTest {
+public class CartServiceTest {
 
     @Mock
     ProductService productService;
